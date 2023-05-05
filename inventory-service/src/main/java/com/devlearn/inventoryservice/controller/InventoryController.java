49c0120ec1c50caf42ec4eeb72cbdea4efbe403a
Devanand Ukalkar <d.ukalkar@gmail.com>
2023-05-05 18:28:35 +0530

    Implemented Inventory Service

---
+++ inventory-service/src/main/java/com/devlearn/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,20 @@
+package com.devlearn.inventoryservice.controller;
+
+import com.devlearn.inventoryservice.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+    @Autowired
+    private InventoryService inventoryService;
+
+    @GetMapping("/{skuCode}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
